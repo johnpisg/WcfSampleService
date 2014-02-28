3c81f6981905c31facba694a1036b97d82ef5ca6
johnpisg <johnpisg@gmail.com>
2014-02-28 16:55:57 -0400

    first sample
    first sample

---
+++ WcfSampleService.Dto/Datos.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace WcfSampleService.Dto
+{
+    [DataContract]
+    public class Datos
+    {
+        private int numero1;
+        private float numero2;
+        private String palabra1;
+
+        [DataMember]
+        public int Numero1
+        {
+            get { return numero1; }
+            set { numero1 = value; }
+        }
+
+        [DataMember]
+        public float Numero2
+        {
+            get { return numero2; }
+            set { numero2 = value; }
+        }
+
+        [DataMember]
+        public String Palabra1
+        {
+            get { return palabra1; }
+            set { palabra1 = value; }
+        }
+
+
+    }
+}
---
+++ wcfsample/WcfSampleService.svc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using WcfSampleService.Dto;
+
+namespace wcfsample
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class WcfSampleService : IWcfSampleService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public Datos GetSampleDatosDto(Datos prueba)
+        {
+            Datos d = new Datos();
+            d.Palabra1 = prueba.Palabra1;
+            d.Numero1 = prueba.Numero1;
+            d.Numero2 = prueba.Numero2;
+            return d;
+
+        }
+    }
+}
